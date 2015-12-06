30a38c375eb07763e07c014400252563f174ea3c
Subrahmanyam83 <gibraltor999@gmail.com>
2015-12-06 22:04:34 +0530

    first

---
+++ src/test/java/Xebia/TestNG1/AppTest.java
@@ -0,0 +1,21 @@
+package Xebia.TestNG1;
+
+import org.testng.annotations.Test;
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+	
+	@Test(enabled =true)
+	public void abc()
+	{
+		
+		System.out.println("Hii Test");
+	}
+}
